87c29a768a7696260c61c930a4b47a6b0cc0626a
nikolovapavlinka <npavlince976@gmail.com>
2024-01-04 19:45:46 +0100

    Final version 2

--- script.js
+++ script.js
@@ -16,15 +16,6 @@ $(document).ready(function () {
 $(document).ready(function() {
   $("#basic").click(function(e) {
     $("#selected_plan").val("Basic");
-<<<<<<< HEAD
-    $("#basic").css("border", "3px solid blue");
-    $("#basic").css("box-shadow", "0px 7px 5px 0px #3033e3");
-
-    $("#standard").css("border", "3px solid #777373");
-    $("#standard").css("box-shadow", "0px 3px 1px 0px #dedede");
-
-    $("#premium").css("border", "3px solid #777373");
-=======
     $("#basic").css("border", "1px solid blue");
     $("#basic").css("box-shadow", "0px 7px 5px 0px #3033e3");
 
@@ -32,7 +23,6 @@ $(document).ready(function() {
     $("#standard").css("box-shadow", "0px 3px 1px 0px #dedede");
 
     $("#premium").css("border", "1px solid #dedede");
->>>>>>> 9de7772352d47274a0d8ebbb336d628ed0916844
     $("#premium").css("box-shadow", "0px 3px 1px 0px #dedede");
   })
 });
@@ -40,15 +30,6 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("#standard").click(function(e) {
     $("#selected_plan").val("Standard");
-<<<<<<< HEAD
-    $("#standard").css("border", "3px solid blue");
-    $("#standard").css("box-shadow", "0px 7px 5px 0px #3033e3");
-
-    $("#basic").css("border", "3px solid #777373");
-    $("#basic").css("box-shadow", "0px 3px 1px 0px #dedede");
-
-    $("#premium").css("border", "3px solid #777373");
-=======
     $("#standard").css("border", "1px solid blue");
     $("#standard").css("box-shadow", "0px 7px 5px 0px #3033e3");
 
@@ -56,7 +37,6 @@ $(document).ready(function() {
     $("#basic").css("box-shadow", "0px 3px 1px 0px #dedede");
 
     $("#premium").css("border", "1px solid #dedede");
->>>>>>> 9de7772352d47274a0d8ebbb336d628ed0916844
     $("#premium").css("box-shadow", "0px 3px 1px 0px #dedede");
   })
 });
@@ -64,15 +44,6 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("#premium").click(function(e) {
     $("#selected_plan").val("Premium");
-<<<<<<< HEAD
-    $("#premium").css("border", "3px solid blue");
-    $("#premium").css("box-shadow", "0px 7px 5px 0px #3033e3");
-
-    $("#standard").css("border", "3px solid #777373");
-    $("#standard").css("box-shadow", "0px 3px 1px 0px #dedede");;
-
-    $("#basic").css("border", "3px solid #777373");
-=======
     $("#premium").css("border", "1px solid blue");
     $("#premium").css("box-shadow", "0px 7px 5px 0px #3033e3");
 
@@ -80,7 +51,6 @@ $(document).ready(function() {
     $("#standard").css("box-shadow", "0px 3px 1px 0px #dedede");;
 
     $("#basic").css("border", "1px solid #dedede");
->>>>>>> 9de7772352d47274a0d8ebbb336d628ed0916844
     $("#basic").css("box-shadow", "0px 3px 1px 0px #dedede");
   })
 });
